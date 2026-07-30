aee8895aa206fcbb2a6ac60bd1ffe6d552611943
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2026-07-30 19:56:33 +0600

    code improvements

--- projects.py
+++ projects.py
@@ -12,11 +12,12 @@ def load_project_task(project_tasks_file_path: pathlib.Path, project_name: str):
     collection.add_collection(module.collection, name=project_name)
 
 
-# TODO: move 'C:\MyLife\SWP\Projects' into env-variable
-load_project_task(pathlib.Path(r"C:\MyLife\SWP\Projects\Тесты\automated.py"), "tests")
-load_project_task(pathlib.Path(r"C:\MyLife\SWP\Projects\МАСТЕРСКАЯ\automated.py"), "work_shop")
-load_project_task(pathlib.Path(r"C:\MyLife\SWP\Projects\МАСТЕРСКАЯ\Инженерная-Стенка\V4\automated.py"), "mm_v_iv")
-load_project_task(pathlib.Path(r"C:\MyLife\SWP\Projects\МАСТЕРСКАЯ\Кинематический-Верстак\Z-Лифт\V3\automated.py"), "z_lift_v_iii")
-load_project_task(pathlib.Path(r"C:\MyLife\SWP\Projects\МАСТЕРСКАЯ\Запор-Для-Ворот\automated.py"), "gate_latch")
-load_project_task(pathlib.Path(r"C:\MyLife\SWP\Projects\МАСТЕРСКАЯ\Кинематический-Верстак\V3\automated.py"), "kinematic_workbench_v_iii")
-load_project_task(pathlib.Path(r"C:\MyLife\SWP\Projects\МАСТЕРСКАЯ\Держатель-Для-Сварочной-Горелки\automated.py"), "welding_torch_holder")
+SWP_FOLDER = pathlib.Path("C:/MyLife/SWP/Projects")
+
+load_project_task(SWP_FOLDER / "Тесты/automated.py", "tests")
+load_project_task(SWP_FOLDER / "МАСТЕРСКАЯ/Верстак-Dim1000x600x50/automated.py", "workbench_1000_600_50")
+load_project_task(SWP_FOLDER / "МАСТЕРСКАЯ/Инженерная-Стенка/V4/automated.py", "mm_v_iv")
+load_project_task(SWP_FOLDER / "МАСТЕРСКАЯ/Кинематический-Верстак/Z-Лифт/V3/automated.py", "z_lift_v_iii")
+load_project_task(SWP_FOLDER / "МАСТЕРСКАЯ/Запор-Для-Ворот/automated.py", "gate_latch")
+load_project_task(SWP_FOLDER / "МАСТЕРСКАЯ/Кинематический-Верстак/V3/automated.py", "kinematic_workbench_v_iii")
+load_project_task(SWP_FOLDER / "МАСТЕРСКАЯ/Держатель-Для-Сварочной-Горелки/automated.py", "welding_torch_holder")
--- utils/doc_creator/iso_elements.py
+++ utils/doc_creator/iso_elements.py
@@ -19,32 +19,18 @@ def detect_link(name: str, type: str) -> str:
 ISO_ELEMENTS = {
     'countersunk flat head cross recess': {
         'translation': 'винт с потайной головкой и крестообразным шлицем',
-        'type_to_links': {
-            'ISO 7046-1 - M8 x 35 - Z - 35N':
-            'https://www.ozon.ru/product/vint-m8-h-35-mm-potaynoy-30-sht-1804939507/?at=oZt6npq0Bh2XrnG6sQWXY83sN1zyrEU9nZ4lYI4yn4DE&sh=w7z-KQgUsg'
-        }
+        'type_to_links': {}
     },
     'hex bolt': {
         'translation': 'болт шестигранный',
-        'type_to_links': {
-            'ISO 4014 - M10 x 70 x 26-N':
-            'https://www.ozon.ru/product/bolt-m10-h-70-mm-1kg-shestigrannyy-otsinkovannyy-din-933-2545673143/?at=6WtZNlqLNu15DBpkU1XkqJ5TXg366wI5jLRwJh7N7w7W&sh=w7z-KazKlQ'
-        }
+        'type_to_links': {}
     },
     'hex nut': {
         'translation': 'гайка шестигранная',
-        'type_to_links': {
-            'ISO - 4032 - M10 - W - N':
-            'https://www.ozon.ru/product/gayka-shestigrannaya-m10-100sht-din-934-otsinkovannaya-2967340027/?at=mqtkxL9yxFZyE2j0cGp8j9OSNW3yz1hGGxBVpf2ko4Dj&sh=w7z-KazKlQ'
-        }
+        'type_to_links': {}
     },
     'hex flange nut': {
         'translation': 'гайка шестигранная c фланцем',
-        'type_to_links': {
-            'ISO - 4161 - M8 - N':
-            'https://www.ozon.ru/product/gayka-s-flantsem-m10-otsinkovannaya-10-sht-2558956778/?at=ywtA1mpO1FZmw4kjCR2x2WAC4r2Y9PSlDXzPMuDg51jx&sh=w7z-KazKlQ',
-            'ISO - 4161 - M10 - N':
-            'https://www.ozon.ru/product/gayka-s-flantsem-m10-otsinkovannaya-10-sht-2558956778/?at=ywtA1mpO1FZmw4kjCR2x2WAC4r2Y9PSlDXzPMuDg51jx&sh=w7z-KazKlQ'
-        }
+        'type_to_links': {}
     }
 }
